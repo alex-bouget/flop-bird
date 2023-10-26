b1f4ade6065c77864fcdee14b7d366123983187c
BOUGET Alexandre <abouget68@gmail.com>
2023-10-26 19:00:09 +0200

    begin cpp

--- flop_bird.cpp
+++ flop_bird.cpp
@@ -1,8 +1,25 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    window.setFramerateLimit(60);
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+    
 
     return 0;
 }
